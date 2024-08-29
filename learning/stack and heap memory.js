c1cadb487e4223d62c7ef7660ac48927dc957c1b
Mr-Kripesh <udhayapanta@gmail.com>
2024-08-29 18:20:08 +0000

    memory

---
+++ learning/stack and heap memory.js
@@ -0,0 +1,19 @@
+// Stack (Primitive)
+// * Primitive data type and refernces
+// * Size is known at compile Time
+// * Fixed memory allocated
+
+
+// Heap (Non- Primitive)
+// * Objects and function 
+// * Size is known at run Time
+// * No limit for object memory
+
+
+let myname = "Kripesh raj panta";
+let anothername = myname;
+anothername = "Udhaya Raj Panta";
+
+console.log(anothername);
+console.log(myname);
+
