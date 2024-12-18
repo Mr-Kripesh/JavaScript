a1ccc9bc8e734abbf3a2d272bd57e860f6c1f138
unknown <udhayapanta@gmail.com>
2024-12-17 19:37:03 -0600

    I complete JavaScript Booleans and If-statements

---
+++ learning/day15exercise.js
@@ -0,0 +1,22 @@
+let hour = 17 ;
+const name = 'Kripesh Raj Panta';
+if (hour >= 6 && hour<=12){
+    console.log(`Good Morning ${name}`);
+}
+else if(hour >= 13 && hour <=17){
+    console.log(`Good Afternoon ${name}`);
+}
+else{
+    console.log(`Good Night ${name}`);
+}
+
+
+
+
+
+
+
+
+
+
+//node learning/day15exercise.js
