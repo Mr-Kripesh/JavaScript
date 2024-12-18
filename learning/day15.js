49d01a063a95db4fec230b9b9839e7431bca8393
unknown <udhayapanta@gmail.com>
2024-12-18 01:09:30 -0600

    Function Half

--- learning/day15.js
+++ learning/day15.js
@@ -3,31 +3,31 @@
 
 // <p>Rock Paper Scisssors</p>
 // <button onclick="
-//     let computerMove = '';
-//     let randomNumber = Math.random();
-//     if (randomNumber >= 0 && randomNumber < 1/3){
-//     computerMove = 'rock';
-//     } 
-//     else if (randomNumber >= 1/3 && randomNumber < 2/3 ){
-//         computerMove = 'paper';
-//         }
-//     else if (randomNumber >= 2/3 && randomNumber <1){
-//     computerMove = 'scissors';
-//     }
+    // let computerMove = '';
+    // let randomNumber = Math.random();
+    // if (randomNumber >= 0 && randomNumber < 1/3){
+    // computerMove = 'rock';
+    // } 
+    // else if (randomNumber >= 1/3 && randomNumber < 2/3 ){
+    //     computerMove = 'paper';
+    //     }
+    // else if (randomNumber >= 2/3 && randomNumber <1){
+    // computerMove = 'scissors';
+    // }
 
-//     let result ='';
+    // let result ='';
 
-//     if (computerMove === 'rock'){
-//     result = 'Tie';
-//     }
-//     else if(computerMove === 'paper'){
-//     result = 'You lose.';
-//     }
-//     else if(computerMove === 'scissors'){
-//     result = 'You Win.';
-//     }
+    // if (computerMove === 'rock'){
+    // result = 'Tie';
+    // }
+    // else if(computerMove === 'paper'){
+    // result = 'You lose.';
+    // }
+    // else if(computerMove === 'scissors'){
+    // result = 'You Win.';
+    // }
     
-//     alert(`You picked rock. Computer Picked ${computerMove}. So ${result} `);
+    // alert(`You picked rock. Computer Picked ${computerMove}. So ${result} `);
 
 // ">Rock</button>
 
--- learning/day15exercise.js
+++ learning/day15exercise.js
@@ -10,21 +10,27 @@
 //     console.log(`Good Night ${name}`);
 // }
 
-let age = (Math.floor(Math.random()*100)+1);
-let day = (Math.floor(Math.random()*7)+1);
-let result = '';
- if ((age <= 6 || age >= 65) && day != 1 && day != 7){
-    console.log('You are qualified for getting discount.');
-    console.log(age);
-    console.log(day);
+
+
+// let age = (Math.floor(Math.random()*100)+1);
+// let day = (Math.floor(Math.random()*7)+1);
+
+//  if ((age <= 6 || age >= 65) && day != 1 && day != 7){
+//     console.log('You are qualified for getting discount.');
+//     console.log(age);
+//     console.log(day);
     
- }
- else{
-    console.log(age);
-    console.log(day);
-    console.log("Sorry! You are not eligible to get the discount");
-result = input("Enter the day")
- }
+//  }
+//  else{
+//     console.log(age);
+//     console.log(day);
+//     console.log("Sorry! You are not eligible to get the discount");
+//  }
+
+
+
+
+
 
 
 
